11f92ab0699961e15932864c77de89ca82311b85
Charles <peiyong.liu@mail.mcgill.ca>
2020-03-14 19:52:00 -0400

    done question 1 and 2

--- Assignment 2/fft.py
+++ Assignment 2/fft.py
@@ -6,39 +6,40 @@ import os
 
 
 def DFT_slow(x):
-    # """Compute the discrete Fourier Transform of the 1D array x"""
-    # x = np.asarray(x, dtype=float)
-    # N = x.shape[0]
-    # n = np.arange(N)
-    # k = n.reshape((N, 1))
-    # M = np.exp(-2j * np.pi * k * n / N)
-    # return np.dot(M, x)
-    t = []
-    N = len(x)
-    for k in range(N):
-        a = 0
-        for n in range(N):
-            a += x[n]*np.exp(-2j*np.pi*k*n*(1/N))
-        t.append(a)
-
-    # round(num.real, 2) + round(num.imag, 2) * 1j
-    t = list(map(lambda temp: round(temp.real, 2) + round(temp.imag, 2) * 1j, t))
-    return np.asarray(t)
+    """Compute the discrete Fourier Transform of the 1D array x"""
+    x = np.asarray(x, dtype=float)
+    N = x.shape[0]
+    n = np.arange(N)
+    k = n.reshape((N, 1))
+    M = np.exp(-2j * np.pi * k * n / N)
+    return np.dot(M, x)
+    # t = []
+    # N = len(x)
+    # for k in range(N):
+    #     a = 0
+    #     for n in range(N):
+    #         a += x[n]*np.exp(-2j*np.pi*k*n*(1/N))
+    #     t.append(a)
+    # return np.asarray(t)
 
 
-def IDFT_slow(t):
-    x = []
-    N = len(t)
-    for n in range(N):
-        a = 0
-        for k in range(N):
-            a += t[k]*np.exp(2j*np.pi*k*n*(1/N))
-        a /= N
-        x.append(a)
-
-    # round(num.real, 2) + round(num.imag, 2) * 1j
-    x = list(map(lambda temp: round(temp.real, 2) + round(temp.imag, 2) * 1j, x))
-    return np.asarray(x)
+def IDFT_slow(x):
+    """Compute the inverse discrete Fourier Transform of the 1D array x"""
+    x = np.asarray(x, dtype=float)
+    N = x.shape[0]
+    n = np.arange(N)
+    k = n.reshape((N, 1))
+    M = np.exp(2j * np.pi * k * n / N)
+    return np.dot(M, x)
+    # x = []
+    # N = len(t)
+    # for n in range(N):
+    #     a = 0
+    #     for k in range(N):
+    #         a += t[k]*np.exp(2j*np.pi*k*n*(1/N))
+    #     a /= N
+    #     x.append(a)
+    # return np.asarray(x)
 
 
 def FFT(x):
@@ -54,11 +55,10 @@ def FFT(x):
         X_even = FFT(x[::2])
         X_odd = FFT(x[1::2])
         factor = np.exp(-2j * np.pi * np.arange(N) / N)
-        return np.concatenate([X_even + factor[:N / 2] * X_odd,
-                               X_even + factor[N / 2:] * X_odd])
+        return np.concatenate([X_even + factor[:int(N / 2)] * X_odd,
+                               X_even + factor[int(N / 2):] * X_odd])
 
 
-# TODO: Wrong
 def FFT_inverse(x):
     x = np.asarray(x, dtype=float)
     N = x.shape[0]
@@ -71,9 +71,16 @@ def FFT_inverse(x):
         X_even = FFT_inverse(x[::2])
         X_odd = FFT_inverse(x[1::2])
         factor = np.exp(2j * np.pi * np.arange(N) / N)
-        # print("stack: " + str(len(inspect.stack(0))))
-        return np.concatenate([X_even + factor[:N / 2] * X_odd,
-                               X_even + factor[N / 2:] * X_odd])
+        return np.concatenate([X_even + factor[:int(N / 2)] * X_odd,
+                               X_even + factor[int(N / 2):] * X_odd])
+
+
+def IDFT(x):
+    return list(map(lambda a: a / len(x), IDFT_slow(x)))
+
+
+def IFFT(x):
+    return list(map(lambda a: a / len(x), FFT_inverse(x)))
 
 
 mode = 1
@@ -91,12 +98,12 @@ if __name__ == '__main__':
 
     # Case 2: Two inputs: mode and address
     elif len(sys.argv) == 3:
-        try:    # Is this a number?
+        try:  # Is this a number?
             mode = int(sys.argv[1])
         except ValueError:
             print("Invalid mode input! Mode should be inputted 1, 2, 3, or 4!\tYour input: " + sys.argv[1])
             exit(1)
-        else:   # Check if mode is in range of [1, 4]
+        else:  # Check if mode is in range of [1, 4]
             if not 1 <= mode <= 4:
                 print("Invalid mode input! Mode should be inputted 1, 2, 3, or 4!\tYour input: " + sys.argv[1])
                 exit(1)
@@ -110,7 +117,7 @@ if __name__ == '__main__':
 
     # Case 3: One input: either mode or address
     else:
-        try:    # Is this a number?
+        try:  # Is this a number?
             mode = int(sys.argv[1])
         except ValueError:  # If not, is this a valid address?
             if not os.path.isfile(sys.argv[1]):
@@ -124,16 +131,6 @@ if __name__ == '__main__':
                 print("Invalid mode input! Mode should be inputted 1, 2, 3, or 4!\tYour input: " + sys.argv[1])
                 exit(1)
 
-        arr = [1, 4, 3, 2]
-        arr_dft = DFT_slow(arr)
-        print(type(arr_dft))    # <class 'numpy.complex128'>
-        print(arr_dft)
-
-        arr_fft = FFT(arr)
-        print(type(arr_fft))
-        print(arr_fft)
-        # arr_ifft = IDFT_slow(arr_fft)
-        # print(arr_ifft)
-
-
-
+        x = np.random.random(1024)
+        print(np.allclose(FFT(x), np.fft.fft(x)))
+        print(np.allclose(IFFT(x), np.fft.ifft(x)))
