2d063d286d69f90267972f82705c71fb783f07d2
Charles <peiyong.liu@mail.mcgill.ca>
2020-03-21 19:53:44 -0400

    finish 2d

--- Assignment 2/fft.py
+++ Assignment 2/fft.py
@@ -1,179 +1,174 @@
-import inspect
-import cmath
 import numpy as np
-from math import log, ceil
 import sys
 import os
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
-from scipy import fftpack
+import cv2
 from matplotlib.colors import LogNorm
 
 
-def DFT_slow(x):
-    """Compute the discrete Fourier Transform of the 1D array x"""
-    x = np.asarray(x, dtype=float)
-    N = x.shape[0]
-    n = np.arange(N)
-    k = n.reshape((N, 1))
-    M = np.exp(-2j * np.pi * k * n / N)
-    return np.dot(M, x)
-    # t = []
-    # N = len(x)
-    # for k in range(N):
-    #     a = 0
-    #     for n in range(N):
-    #         a += x[n]*np.exp(-2j*np.pi*k*n*(1/N))
-    #     t.append(a)
-    # return np.asarray(t)
+def DFT_slow(inputArray):
+    N = inputArray.size
+    V = np.array([[np.exp(-2j * np.pi * v * y / N) for v in range(N)] for y in range(N)])
+    return inputArray.dot(V)
 
 
-def IDFT_slow(x):
-    """Compute the inverse discrete Fourier Transform of the 1D array x"""
-    x = np.asarray(x, dtype=float)
-    N = x.shape[0]
-    n = np.arange(N)
-    k = n.reshape((N, 1))
-    M = np.exp(2j * np.pi * k * n / N)
-    return np.dot(M, x)
-    # x = []
-    # N = len(t)
-    # for n in range(N):
-    #     a = 0
-    #     for k in range(N):
-    #         a += t[k]*np.exp(2j*np.pi*k*n*(1/N))
-    #     a /= N
-    #     x.append(a)
-    # return np.asarray(x)
+def IDFT_slow(inputArray):
+    N = inputArray.size
+    V = np.array([[np.exp(2j * np.pi * v * y / N) for v in range(N)] for y in range(N)])
+    return inputArray.dot(V)
 
 
-def FFT(x):
-    """A recursive implementation of the 1D Cooley-Tukey FFT"""
-    x = np.asarray(x, dtype=float)
-    N = x.shape[0]
+def FFT(inputArray):
+    inputArray = np.asarray(inputArray, dtype=float)
+    N = inputArray.shape[0]
 
     if N % 2 > 0:
         raise ValueError("size of x must be a power of 2")
     elif N <= 32:  # this cutoff should be optimized
-        return DFT_slow(x)
+        return DFT_slow(inputArray)
     else:
-        X_even = FFT(x[::2])
-        X_odd = FFT(x[1::2])
+        X_even = FFT(inputArray[::2])
+        X_odd = FFT(inputArray[1::2])
         factor = np.exp(-2j * np.pi * np.arange(N) / N)
-        return np.concatenate([X_even + factor[:int(N / 2)] * X_odd,
-                               X_even + factor[int(N / 2):] * X_odd])
+        return np.concatenate([X_even + factor[:int(N / 2)] * X_odd, X_even + factor[int(N / 2):] * X_odd])
 
 
-def FFT_inverse(x):
-    x = np.asarray(x, dtype=float)
-    N = x.shape[0]
+def FFT_inverse(inputArray):
+    inputArray = np.asarray(inputArray, dtype=complex)
+    N = inputArray.shape[0]
 
     if N % 2 > 0:
         raise ValueError("size of x must be a power of 2")
     elif N <= 32:  # this cutoff should be optimized
-        return IDFT_slow(x)
+        return IDFT_slow(inputArray)
     else:
-        X_even = FFT_inverse(x[::2])
-        X_odd = FFT_inverse(x[1::2])
+        X_even = FFT_inverse(inputArray[::2])
+        X_odd = FFT_inverse(inputArray[1::2])
         factor = np.exp(2j * np.pi * np.arange(N) / N)
-        return np.concatenate([X_even + factor[:int(N / 2)] * X_odd,
-                               X_even + factor[int(N / 2):] * X_odd])
+        return np.concatenate([X_even + factor[:int(N / 2)] * X_odd, X_even + factor[int(N / 2):] * X_odd])
 
 
-def IDFT(x):
-    return list(map(lambda a: a / len(x), IDFT_slow(x)))
+def FFT_2D(input2DArray):
+    N = input2DArray.shape[1]
+    if N % 2 > 0:
+        raise ValueError("size of x must be a power of 2")
+    elif N <= 32:  # this cutoff should be optimized
+        return np.array([DFT_slow(input2DArray[i, :]) for i in range(input2DArray.shape[0])])
+    else:
+        X_even = FFT_2D(input2DArray[:, ::2])
+        X_odd = FFT_2D(input2DArray[:, 1::2])
+        factor = np.array([np.exp(-2j * np.pi * np.arange(N) / N) for i in range(input2DArray.shape[0])])
+        return np.hstack([X_even + np.multiply(factor[:, :int(N / 2)], X_odd),
+                          X_even + np.multiply(factor[:, int(N / 2):], X_odd)])
 
 
-def IFFT(x):
-    return list(map(lambda a: a / len(x), FFT_inverse(x)))
+def IFFT_2D(input2DArray):
+    N = input2DArray.shape[1]
+    if N % 2 > 0:
+        raise ValueError("size of x must be a power of 2")
+    elif N <= 32:  # this cutoff should be optimized
+        return np.array([IDFT_slow(input2DArray[i, :]) for i in range(input2DArray.shape[0])])
+    else:
+        X_even = IFFT_2D(input2DArray[:, ::2])
+        X_odd = IFFT_2D(input2DArray[:, 1::2])
+        factor = np.array([np.exp(2j * np.pi * np.arange(N) / N) for i in range(input2DArray.shape[0])])
+        return np.hstack([X_even + np.multiply(factor[:, :int(N / 2)], X_odd),
+                          X_even + np.multiply(factor[:, int(N / 2):], X_odd)])
 
 
-def twoFFT(x):
-    """ x is inported and modified as a 2-d array"""
-    m, n = x.shape
-    return np.array([ [ sum([ sum([ x[i,j]*np.exp(-1j*2*np.pi*(k_m*i/m + k_n*j/n)) for i in range(m) ]) for j in range(n) ]) for k_n in range(n) ] for k_m in range(m) ])
-        
+def IDFT(inputArray):
+    return list(map(lambda a: a / len(inputArray), IDFT_slow(inputArray)))
 
 
-def twoIFFT(x):
-    m, n = x.shape
-    for k in range(m):
-        for l in range(n):
-            x[k,l] = sum([ sum([ x[i,j]*np.exp(j*2*np.pi*(k*i/m + l*j/n)) for i in range(m) ]) for j in range(n)])
-    return x   
+def IFFT(inputArray):
+    return list(map(lambda a: a / len(inputArray), FFT_inverse(inputArray)))
 
 
-mode = 0
-address = "moonlanding.png"
+def twoD_FFT(input2DArray):
+    input2DArray = np.asarray(input2DArray, dtype=float)
+    return FFT_2D(FFT_2D(input2DArray).T).T
 
-def plot_spectrum(im_fft):
-    from matplotlib.colors import LogNorm
-    # A logarithmic colormap
-    plt.imshow(np.abs(im_fft), norm=LogNorm(vmin=5))
-    plt.colorbar()
+
+def twoD_IFFT(input2DArray):
+    input2DArray = np.asarray(input2DArray, dtype=complex)
+    return np.array(
+        list(map(lambda a: a / (len(input2DArray) * len(input2DArray[0])), IFFT_2D(IFFT_2D(input2DArray).T).T)))
+
+
+def next_power_of_2(x):
+    return 1 if x == 0 else 2 ** (x - 1).bit_length()
+
 
 # mode manipulation
 def modeOutput(mode, address):
-    if(mode == 1):
+    # img is a 2-d array
+    img_original = cv2.imread(address, cv2.IMREAD_UNCHANGED)
+    # img_original = mpimg.imread(address)
+    vertical = img_original.shape[0]
+    horizontal = img_original.shape[1]
+    changed_vertical = next_power_of_2(vertical)
+    changed_horizontal = next_power_of_2(horizontal)
+
+    dsize = (changed_horizontal, changed_vertical)
+    img_original = cv2.resize(img_original, dsize, interpolation=cv2.INTER_AREA)
+
+    if mode == 1:
         print("mode 1 is triggered")
 
-        # img is a 2-d array 
-        img_original = mpimg.imread(address)
+        # my function
+        # img_FFT = twoD_FFT(img_original)
 
-        #img_FFT = twoFFT(img_original)
-        img_FFT = fftpack.fft2(img_original)
+        # built-in function
+        img_FFT = np.fft.fft2(img_original)
 
         plt.figure("Mode_1")
         plt.subplot(211)
         plt.imshow(img_original)
-        
+
         plt.subplot(212)
         plt.imshow(np.abs(img_FFT), norm=LogNorm(vmin=5))
         plt.show()
 
-    if(mode == 2):
+    if mode == 2:
         print("mode 2 is triggered")
         keep_fraction = 0.09
 
         # Call ff a copy of the original transform. Numpy arrays have a copy
         # method for this purpose.
-        img_original = mpimg.imread(address)
 
-        im_fft2 = fftpack.fft2(img_original)
+        # my function
+        im_fft2 = twoD_FFT(img_original)
 
+        # built-in function
+        #im_fft2 = np.fft.fft2(img_original)
 
-        # Set r and c to be the number of rows and columns of the array.
         r, c = im_fft2.shape
-
-        # Set to zero all rows with indices between r*keep_fraction and
-        # r*(1-keep_fraction):
         im_fft2[int(r*keep_fraction):int(r*(1-keep_fraction))] = 0.0
-
-        # Similarly with the columns:
         im_fft2[:, int(c*keep_fraction):int(c*(1-keep_fraction))] = 0.0
 
-        #plt.figure()
-        img_new = fftpack.ifft2(im_fft2).real
+        #img_new = np.fft.ifft2(im_fft2).real
+        img_new = twoD_IFFT(im_fft2).real
+
         plt.figure("Mode_2")
         plt.subplot(211)
         plt.imshow(img_original)
-        
+
         plt.subplot(212)
         plt.imshow(img_new)
         plt.show()
 
-    if(mode == 3):
-        T = 0.1
-        c = F3 * (P3 >= T)
-        fM = ifft2(c)*W*H
-        plt.imshow(np.abs(fM));    
-
-
-
-
+    # if (mode == 3):
+    # T = 0.1
+    # c = F3 * (P3 >= T)
+    # fM = ifft2(c) * W * H
+    # plt.imshow(np.abs(fM));
 
 
 if __name__ == '__main__':
+    mode = 1
+    address = "moonlanding.png"
+
     # If there are more than two inputs, exit
     if len(sys.argv) > 3:
         print("Invalid inputs!\tpython fft.py [-m mode] [-i image]")
@@ -202,10 +197,7 @@ if __name__ == '__main__':
         else:
             address = sys.argv[2]
 
-        """ function call """
-        modeOutput(mode, address)    
-
-    # Case 3: One input: either mode or address
+        # Case 3: One input: either mode or address
     else:
         try:  # Is this a number?
             mode = int(sys.argv[1])
@@ -221,6 +213,4 @@ if __name__ == '__main__':
                 print("Invalid mode input! Mode should be inputted 1, 2, 3, or 4!\tYour input: " + sys.argv[1])
                 exit(1)
 
-        x = np.random.random(1024)
-        print(np.allclose(FFT(x), np.fft.fft(x)))
-        print(np.allclose(IFFT(x), np.fft.ifft(x)))
+    modeOutput(mode, address)
